f0cfc52ab0a112d118202dc4a7e924b6b22145e3
royfini <138002571+royfini@users.noreply.github.com>
2024-03-05 19:07:42 +0200

    complete sugnup page funct.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
