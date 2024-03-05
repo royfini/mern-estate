3c979401520412f62d7532facb17a4bd3895e903
royfini <138002571+royfini@users.noreply.github.com>
2024-03-05 16:48:19 +0200

    complete sign-up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,6 +9,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json("User created successfully!");
   } catch (error) {
-next(error);
-}
+    next(error);
+  }
 };
