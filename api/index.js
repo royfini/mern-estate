d7a5acfd6dc859c1580da75fb8acd59319007c21
royfini <138002571+royfini@users.noreply.github.com>
2024-03-26 18:52:25 +0200

    create user update api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
