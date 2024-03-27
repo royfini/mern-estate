b76bf09a43b0773dbf51ea67c1698117ef7dff4c
royfini <138002571+royfini@users.noreply.github.com>
2024-03-27 10:37:00 +0200

    create listing backend

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('DATABASE CONNECTED SUCCESFULLY');
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("DATABASE CONNECTED SUCCESFULLY");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -20,19 +24,20 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use((err, req, res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
